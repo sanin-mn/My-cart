8ade19d04f85d0beef16616d7e767f6c6544e525
sanin-mn <msanin7003@gmail.com>
2023-11-27 21:41:01 +0530

    initial commit design completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // all product
+  {
+    path: '', component: AllProductsComponent
+  },
+  // login
+  {
+    path: 'user/login', component: LoginComponent
+  },
+  // register
+  {
+    path: 'user/register', component: RegisterComponent
+  },
+  // whishlist
+  {
+    path: 'wishlist', component: WishlistComponent
+  },
+  // cart
+  {
+    path: 'cart', component: CartComponent
+  },
+  // checkout
+  {
+    path: 'user/checkout', component: CheckoutComponent
+  },
+  // view product
+  {
+    path: 'product/view/:id', component: ViewProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
